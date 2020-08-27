6881e78dcfd942d781bf08b23df196b853f35b2d
gramm500 <nikul.vi@yandex.ru>
2020-08-27 10:46:36 +0300

    minor changes for heroku

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+const port = process.env.PORT || 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const multer = require("multer");
 const app = express();
 app.set("view engine", "ejs");
-
+const upload = multer();
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(upload.array());
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.render("home");
+    res.send("home");
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("server running on port 3000")
 })
